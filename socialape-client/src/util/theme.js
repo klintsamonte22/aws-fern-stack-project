52d4a33bc12a6ddb0ebf0e5dd153b4b716dedd5c
Klint Cyril Samonte <klint.samonte@awsys-i.com>
2020-07-09 20:51:46 +0800

    socialape client

---
+++ socialape-client/src/util/theme.js
@@ -0,0 +1,45 @@
+export default {
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+  spreadThis: {
+    typography: {
+      useNextVariants: true,
+    },
+    form: {
+      textAlign: "center",
+    },
+    image: {
+      margin: "20px auto 20px auto",
+    },
+    pageTitle: {
+      margin: "10px auto 10px auto",
+    },
+    textField: {
+      margin: "10px auto 10px auto",
+    },
+    button: {
+      margin: 20,
+      position: "relative",
+    },
+    customError: {
+      color: "red",
+      fontSize: "0.8rem",
+      marginTop: 10,
+    },
+    progress: {
+      position: "absolute",
+    },
+  },
+};
